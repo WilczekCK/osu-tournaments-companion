db9fe232a2e50f323f97bc1b08208c12bc25b41b
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-11-17 14:41:49 +0100

    init structure of osuapi controller

--- api/src/controllers/osuApi.ts
+++ api/src/controllers/osuApi.ts
@@ -1,5 +1,30 @@
 const osuApi = {
-    //...
+    init: _ => new Auth(12345, 'test', 'localhost:3000'),
+    getUserInfo: (cb) => new User(),
+    getRoomInfo: (cb) => new Room(),
+}
+
+class Auth{
+    constructor(
+        private clientId: number,
+        private clientKey: string,
+        private callbackUrl: string,
+    ){}
+}
+class User{
+    constructor(
+        //... waiting for api config
+    ){}
+}
+class Room{
+    constructor(
+        //... waiting for api config
+    ){}
+}
+class Judge{
+    constructor(
+        //... waiting for api config
+    ){}
 }
 
 export = osuApi;
