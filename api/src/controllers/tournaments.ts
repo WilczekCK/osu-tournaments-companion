782daa4c6ae274446d095fa12ec07f287b4c840a
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-11-17 15:00:07 +0100

    tournaments controller structure init

--- api/src/controllers/tournaments.ts
+++ api/src/controllers/tournaments.ts
@@ -1,5 +1,9 @@
 const tournaments = {
-    //...
+    prepareRoom: _ => _,
+    prepareUserInfo: _ => _,
+    recognizeJudge: _ => _,
+    sendToDb: _ => _,
+    matchInfo: _ => _
 }
 
 export = tournaments;
