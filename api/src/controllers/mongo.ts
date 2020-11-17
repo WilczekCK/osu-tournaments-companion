89e69ab130502a6e50decece9d91ce6af1d2723b
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-11-17 14:54:05 +0100

    mongo init structure

--- api/src/controllers/mongo.ts
+++ api/src/controllers/mongo.ts
@@ -1,5 +1,20 @@
 const mongo = {
-    //...
+    client: {},
+    init: function() { this.client = new Auth('localhost', 'root', 'rootpass', 'osucompanion') },
+    get: _ => _,
+    insert: _ => _,
+    delete: _ => _,
+    modify: _ => _,
+}
+
+class Auth{
+    constructor(
+        private host: string,
+        private user: string,
+        private pass: string,
+        private db: string,
+        private port?: string
+    ){}
 }
 
 export = mongo;
