6a52b5b60d7e8447f454a488b180184710b823d7
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-11-18 12:38:37 +0100

    credentials for mongo provided

--- api/src/controllers/mongo.ts
+++ api/src/controllers/mongo.ts
@@ -1,20 +1,13 @@
-const mongo = {
-    client: {},
-    init: function() { this.client = new Auth('localhost', 'root', 'rootpass', 'osucompanion') },
-    get: _ => _,
-    insert: _ => _,
-    delete: _ => _,
-    modify: _ => _,
+import * as credentials from '../../credentials.json';
+const {mongoCreds} = credentials;
+
+class Mongo{
+    public queryResult: Promise<string>;
+    public getStatus: Promise<number>;
+
+    public constructor (query: string){
+        //this.queryResult 
+    }
 }
 
-class Auth{
-    constructor(
-        private host: string,
-        private user: string,
-        private pass: string,
-        private db: string,
-        private port?: string
-    ){}
-}
-
-export = mongo;
+export = Mongo;
