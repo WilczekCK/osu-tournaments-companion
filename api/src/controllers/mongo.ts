403d6ad1779bdfa1e6af623e4e677de68bb458d8
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-11-18 13:55:54 +0100

    connection to db working + schema

--- api/src/controllers/mongo.ts
+++ api/src/controllers/mongo.ts
@@ -1,12 +1,21 @@
 import * as credentials from '../../credentials.json';
+import mongoose from "mongoose";
 const {mongoCreds} = credentials;
 
-class Mongo{
-    public getConnection : string;
 
-    public constructor (query: string){
-        this.getConnection = 'connected';
-    }
+class Mongo{
+    private uri: string = `${mongoCreds.prefix + mongoCreds.host}:${mongoCreds.port}/${mongoCreds.database}`;
+    public getConnection(){
+        return mongoose.connect(this.uri, 
+            {useUnifiedTopology: true, useNewUrlParser: true},
+            (err: any) => {
+                if(err){
+                    console.log(err.message);
+                }else{
+                    console.log('connected! :)')
+                }
+        })
+    }    
 }
 
 class Query extends Mongo {
@@ -15,11 +24,12 @@ class Query extends Mongo {
     public connection: string;
 
     public constructor (getConnection: string){
-       super(getConnection);
+       super();
     }
 }
 
-const lel = new Query('test')
-console.log(lel)
+
+const mongo = new Mongo();
+mongo.getConnection();
 
 export = Query;
