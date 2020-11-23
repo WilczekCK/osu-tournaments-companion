046c6a2004b37ad163b0f48e0db8d94b538cba57
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-11-23 14:38:50 +0100

    cloned the tournament and made it user controller

---
+++ api/src/controllers/users.ts
@@ -0,0 +1,92 @@
+
+import mongo from "./mongo";
+import usersSchema from '../database/tournaments.schema';
+
+type UpdateSchema = {
+    whereQuery: {
+        [key: string]: string | number,
+    },
+    modifyQuery: {
+        [key: string]: string | number;
+    },
+}
+
+class Users {
+    private connect = () => mongo.getConnection();
+    private disconnect = () => mongo.stopConnection();
+    //@ts-ignore
+    private query = (where: string | number | object) => usersSchema.where(where);
+
+    public displayAll = async () => {
+        this.connect();
+        
+        const result = await usersSchema.find((err: any, tournament: any) => {
+            return tournament;
+        });
+
+        this.disconnect();
+        return result;
+    };
+
+    public displayOne = async (userId: Number) => {
+        this.connect();
+        
+
+        const result = await this.query( {id: userId} ).find((err: any, user: any) => {
+            return user;
+        });
+
+        this.disconnect();
+        return result;
+    };
+
+    public displayCertain = async (whereQuery: Object) => {
+        this.connect();
+        
+        const result = await this.query(whereQuery).find((err: any, user: any) => {
+            return user;
+        });
+
+        this.disconnect();
+        return result;
+    };
+
+    public insert = async (userInfo: Object) => {
+        this.connect()
+
+        const newUser = new usersSchema(userInfo);
+        await newUser.save();
+
+        this.disconnect();
+
+        return {status: 200}
+    };
+
+    public delete = async (userId: Number) => {
+        this.connect()
+
+        const {ok, deletedCount} = await usersSchema.deleteOne({id: userId}, (cb) => cb);
+        
+        this.disconnect();
+
+        const status = ok ? 200 : 400;
+        return {status, deletedCount};
+    }
+
+    public update = async (userInfo: UpdateSchema) => {
+        this.connect()
+
+        const {whereQuery, modifyQuery} = userInfo;
+        const {ok, nModified} = await usersSchema.updateOne(
+            {[whereQuery.prefix]: whereQuery.content}, 
+            {[modifyQuery.prefix]: modifyQuery.content}
+        );
+
+        this.disconnect()
+
+        const status = ok ? 200 : 400;
+        return {status, modified: nModified};
+    }
+}
+
+export = new User();
