19010e70e24486fc17b3dff13ccc6291e25b2c7d
Paweł Wilk <wilkuwdr2008@gmail.com>
2020-11-30 15:16:13 +0100

    insert query for user created

--- api/src/routes/users/manage/index.ts
+++ api/src/routes/users/manage/index.ts
@@ -2,12 +2,24 @@ import Router from 'koa-router';
 import koaBody from 'koa-body';
 import osuApi from '../../../controllers/osuApi';
 import users from '../../../controllers/users';
+import { COPYFILE_EXCL } from 'constants';
 
 const manageUser = new Router({
     prefix: '/m'
 });
     
 manageUser.post('/:id', async (ctx) => {
+    const {id, username, country, playstyle} = await osuApi(`users/${ctx.params.id}/osu`)
+
+    console.log(id, username, country, playstyle)
+    const response = await users.insert({
+        id,
+        username,
+        country: country.code,
+        playStyle: playstyle
+    })
+
+    ctx.body = response;
 })
 
 manageUser.patch('/:id', koaBody(), async (ctx) => {
