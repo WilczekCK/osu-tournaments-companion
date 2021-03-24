c9e3f448b4d5357a354509f83e7af84b8c69c2dc
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-03-24 11:05:20 +0100

    axios get info about the event which did not end in our db

--- api/src/controllers/cron.ts
+++ api/src/controllers/cron.ts
@@ -2,6 +2,7 @@ import osuApi from './osuApi';
 import mongo from './mongo';
 import tournaments from './tournaments';
 import users from './users';
+import axios from 'axios';
 import * as cron from 'node-cron';
 
 class Cron {
@@ -15,6 +16,28 @@ class Cron {
     };
 
     private updateTournaments = async () => {
+        
+        type roomInfo = {
+            detail?: any,
+            game?: any,
+            user_id?: number,
+        }
+
+        for await(let tournament of this.tournamentsToUpdate){
+            const {id} : {id?:number} = tournament;
+            
+            await axios.get(`/tournaments/${id}?osuApi=true`)
+                .then( async ( {data} ) => {
+                    let {match, events, users} = data;
+                    let [{judge}, plays] = await tournaments.parseEventsObject( events );
+
+                    //@ts-ignore
+                    console.log({new:plays, old: tournament.mapsPlayed})
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
         this.isCronInProgress = false;
         return;
     };
@@ -23,8 +46,8 @@ class Cron {
         cron.schedule(`*/${this.secondsEachCron} * * * * *`, async () => {
             
             if( !this.isCronInProgress ){
-                this.prepareToUpdate();
-                this.updateTournaments();
+                await this.prepareToUpdate();
+                await this.updateTournaments();
             }
 
         })
