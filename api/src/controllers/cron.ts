d60e7712d5732e92caf94e291f31ecabcaf2851e
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-03-24 10:11:46 +0100

    30s each cron instead of minute

--- api/src/controllers/cron.ts
+++ api/src/controllers/cron.ts
@@ -6,7 +6,7 @@ import * as cron from 'node-cron';
 
 class Cron {
     public start = async () => {
-        cron.schedule('* * * * *', async () => {
+        cron.schedule('*/30 * * * * *', async () => {
             console.log(await tournaments.displayCertain({'timeEnded': null}));
         })
     }
