785d5eb260151bf874e0085a2f8b7dcbc9dc3d19
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-03-24 10:16:37 +0100

    moved cron seconds to variable + created new ones

--- api/src/controllers/cron.ts
+++ api/src/controllers/cron.ts
@@ -5,8 +5,12 @@ import users from './users';
 import * as cron from 'node-cron';
 
 class Cron {
+    private isInProgress : boolean = false;
+    private secondsEachCron : number = 30;
+    private tournamentsToUpdate : Array<object>;
+    
     public start = async () => {
-        cron.schedule('*/30 * * * * *', async () => {
+        cron.schedule(`*/${this.secondsEachCron} * * * * *`, async () => {
             console.log(await tournaments.displayCertain({'timeEnded': null}));
         })
     }
