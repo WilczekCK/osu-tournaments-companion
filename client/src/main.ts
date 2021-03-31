ae340e92fd60d5390c44436d7901406f385677e8
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-03-31 14:25:24 +0200

    vue-material - no ts support installed

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import { MdMenu, MdContent, MdTabs } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
 
 Vue.config.productionTip = false;
+
+Vue.use(MdMenu)
+Vue.use(MdContent)
+Vue.use(MdTabs)
 
 new Vue({
   router,
