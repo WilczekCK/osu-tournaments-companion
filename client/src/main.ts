483775e658e09a3ab4affb5dcac9e3575eb88baf
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-03-31 14:44:02 +0200

    polishing code + simple comp from vue-material

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,15 +1,16 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
 import { MdMenu, MdContent, MdTabs } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default.css';
 
+import router from './router';
+import App from './App.vue';
+
 Vue.config.productionTip = false;
 
-Vue.use(MdMenu)
-Vue.use(MdContent)
-Vue.use(MdTabs)
+Vue.use(MdMenu);
+Vue.use(MdContent);
+Vue.use(MdTabs);
 
 new Vue({
   router,
