192ebfbaf4770558ca007eb7ac6a60662e48951a
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-03-31 15:12:40 +0200

    divided navbar into other component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,21 @@
 <template lang='pug'>
   #app
-    #nav
-      router-link(to="/")
-        ="Home"
-      router-link(to="/about")
-        ="About"
-      router-view
+    navbar
+    router-view
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import navbar from './components/NavBar.vue';
+
+@Component({
+  components: {
+    navbar,
+  },
+})
+
+export default class Home extends Vue {}
+</script>
 
 <style lang="stylus">
 html
@@ -14,12 +23,12 @@ body
   background #535353
   margin 0
   padding 0
+  width 100%
 
 #app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  width 1280px
+  margin 0 auto
+  display flex
+  flex-direction column
+  align-items center
 </style>
---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+    #nav
+        router-link(to="/")
+            ="Home"
+        router-link(to="/about")
+            ="About"
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class NavBar extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+
+</style>
