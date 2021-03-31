ba30c49b83637544792c5156260e8e2fffef2e04
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-03-31 15:01:20 +0200

    body styling

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,6 +9,12 @@
 </template>
 
 <style lang="stylus">
+html
+body
+  background #535353
+  margin 0
+  padding 0
+
 #app
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
