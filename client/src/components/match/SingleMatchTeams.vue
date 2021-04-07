22d0ff8e7dffaea781045dde48b05bf3d05e4276
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-04-07 14:23:29 +0200

    made the structure of user container

--- client/src/components/match/SingleMatchTeams.vue
+++ client/src/components/match/SingleMatchTeams.vue
@@ -2,8 +2,16 @@
   .teams__container
     .teams__container--red
         .teams__container__member(v-for="item in 5")
-            ='RED'
-            br/
+            .teams__container__member--avatar
+                img(src="https://a.ppy.sh/5286213" alt="user_avatar")
+            .teams__container__member--nickname
+                ="Cookiezi"
+            .teams__container__member--ranking
+                .teams__container__member--ranking--global
+                    ="#2"
+                .teams__container__member--ranking--country
+                    img(src="https://www.countryflags.io/pl/flat/64.png")
+                    ="#1"
     .teams__container--blue
         .teams__container__member(v-for="item in 5")
             ='BLUE'
@@ -27,7 +35,31 @@ export default class Teams extends Vue {
         align-self: space-between
         height: 50px
         background: rgba(0,0,0,.2)
-        color: black
+        color: white
+        font-weight: 800
+        display: flex
+        flex-direction: row
+        align-items: center
+        padding-left: 2.5px
+        &--avatar
+            max-height: 100%
+            width: 45px
+            flex-basis: 10%
+        &--nickname
+            flex-basis: 80%
+            padding-left: 5px
+        &--ranking
+            display: flex
+            flex-basis: 10%
+            flex-direction: column
+            &--global
+                width: 100%
+                text-align: center
+            &--country
+                display: flex
+                flex-direction: row
+                img
+                    width: 45%
     &--red, &--blue
         flex-basis: 50%
         display: flex
