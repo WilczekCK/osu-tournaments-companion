ea4ba09106ffd305e7d335506a103aa574963214
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-04-08 16:27:48 +0200

    stepper does not have icon, but numbers (css magic)

--- client/src/components/match/SingleMatchProgress.vue
+++ client/src/components/match/SingleMatchProgress.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
   .progress__container
-    md-steppers(@md-changed='stepChanged()')
-        md-step(md-label="Match created")
+    md-steppers(md-vertical='true')
+        md-step(md-label="Match created" :md-done="true")
             p="Lorem ipsum dolor sit amet"
-        md-step(md-label="X won")
+        md-step(md-label="X won" :md-done="true")
             p="Lorem ipsum dolor sit amet"
-        md-step(md-label="Y won")
+        md-step(md-label="Y won" :md-done="true")
             p="Lorem ipsum dolor sit amet"
-        md-step(md-label="Y won")
+        md-step(md-label="Y won" :md-done="true")
             p="Lorem ipsum dolor sit amet"
-        md-step(md-label="X won")
+        md-step(md-label="X won" :md-done="true")
             p="Lorem ipsum dolor sit amet"
-        md-step(md-label="X won")
+        md-step(md-label="X won" :md-done="true")
             p="Lorem ipsum dolor sit amet"
-        md-step(md-label="X won the match!")
+        md-step(md-label="X won the match!" :md-done="true")
             p="Lorem ipsum dolor sit amet"
 </template>
 
@@ -22,11 +22,26 @@ import { Vue, Component } from 'vue-property-decorator';
 
 @Component
 export default class Progress extends Vue {
-  stepChanged = (): void => {
-    console.log('xD');
-  }
 }
 </script>
 
 <style lang="sass">
+.progress__container
+    overflow-x: auto
+    height: 100%
+    counter-reset: numbering
+
+/* Stepper icons change to numbers */
+.md-stepper-number
+    &:before
+        position: relative
+        counter-increment: numbering
+        content: " " counter(numbering) " "
+        width: 100%
+        left: 35%
+        color: white
+        background-color: transparent !important
+.md-icon
+    svg
+        display: none
 </style>
