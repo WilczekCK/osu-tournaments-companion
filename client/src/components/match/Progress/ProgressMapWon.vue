661b07741290cb28e72486564be479d2c069d77a
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-04-15 13:48:46 +0200

    temp change of max-length + init of struct of map won component

--- client/src/components/match/Progress/ProgressMapWon.vue
+++ client/src/components/match/Progress/ProgressMapWon.vue
@@ -1,6 +1,17 @@
 <template lang="pug">
     .progress__map__container
-        p {{ mapPlayed }}
+        .progress__map__container__teamWon
+            p
+                b {{ team.name }}
+                = ' team won by'
+                b= ' 400 points!'
+        .progress__map__container__mapInfo
+            h3="Map details"
+            .progress__map__container__mapInfo__columnToRow
+                .progress__map__container__mapInfo__image
+                    img(src="https://assets.ppy.sh/beatmaps/385248/covers/list.jpg?1521103183")
+                .progress__map__container__mapInfo__description
+                    p {{ map.title }}
 </template>
 
 <script lang="ts">
@@ -12,11 +23,31 @@ export default class Progress extends Vue {
 
     @Prop() public map!: Record<string, unknown>;
 
-    teamWon = this.team.name;
+    teamWon = this.team;
 
     mapPlayed = this.map;
 }
 </script>
 
 <style lang="sass">
+    .progress__map__container
+        display: flex
+        flex-direction: column
+        &__teamWon
+            font-size: 1.5em
+            p
+                &:first-letter
+                    text-transform: uppercase
+        &__mapInfo
+            margin-top: -25px
+            h3
+                border-bottom: 2px solid $link-active
+                font-weight: 400
+            &__columnToRow
+                display: flex
+                flex-direction: row
+                &__image
+                    flex-basis: 15%
+                &__description
+                    flex-basis: 77%
 </style>
