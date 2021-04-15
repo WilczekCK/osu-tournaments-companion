16724a73a78dcc311cd5cf2c46b3dbb180dc9ddf
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-04-15 14:12:41 +0200

    match won component (v1) created

--- client/src/components/match/Progress/ProgressMapWon.vue
+++ client/src/components/match/Progress/ProgressMapWon.vue
@@ -9,9 +9,18 @@
             h3="Map details"
             .progress__map__container__mapInfo__columnToRow
                 .progress__map__container__mapInfo__image
-                    img(src="https://assets.ppy.sh/beatmaps/385248/covers/list.jpg?1521103183")
+                    img(src="https://assets.ppy.sh/beatmaps/385248/covers/list@2x.jpg?1521103183")
                 .progress__map__container__mapInfo__description
-                    p {{ map.title }}
+                    .progress__map__container__mapInfo__description--creator
+                        ="Map by: "
+                        span {{ map.creator }}
+                    .progress__map__container__mapInfo__description--artist
+                        span {{ map.artist }}
+                    .progress__map__container__mapInfo__description--title
+                        span {{ map.title }}
+                    .progress__map__container__mapInfo__description--difficulty
+                        ="Difficulty: "
+                        span {{ map.difficulty }}
 </template>
 
 <script lang="ts">
@@ -43,11 +52,33 @@ export default class Progress extends Vue {
             h3
                 border-bottom: 2px solid $link-active
                 font-weight: 400
+                padding-bottom: 5px
             &__columnToRow
                 display: flex
                 flex-direction: row
-                &__image
-                    flex-basis: 15%
-                &__description
-                    flex-basis: 77%
+                @media (max-width: 768px)
+                    flex-direction: column
+            &__image
+                flex-basis: 35%
+                @media (max-width: 768px)
+                    flex-basis: 100%
+            &__description
+                width: 100%
+                display: flex
+                flex-direction: row
+                flex-wrap: wrap
+                align-self: center
+                &--creator
+                    font-size: .8em
+                &--artist
+                    font-size: 1.25em
+                    font-weight: 600
+                    flex-basis: 100%
+                &--title
+                    font-size: 1.1em
+                    font-weight: 600
+                    flex-basis: 100%
+                &--difficulty
+                    span
+                        font-weight: 600
 </style>
