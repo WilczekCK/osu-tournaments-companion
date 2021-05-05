149aead280d07b29b47acf8127a5f61485afff05
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-05-05 12:26:42 +0200

    forgot to git add

---
+++ client/src/components/match/Pagination.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    h1='Paginate'
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+})
+
+export default class SingleMatch extends Vue {
+}
+</script>
+
+<style lang="sass">
+</style>
