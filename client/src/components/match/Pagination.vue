e6a9a48647e6dfc73dc8e2aab5abbad4d4d8d91b
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-05-05 13:19:24 +0200

    looks like it working, changing pages in string

--- client/src/components/match/Pagination.vue
+++ client/src/components/match/Pagination.vue
@@ -1,13 +1,40 @@
 <template lang="pug">
-    h1='Paginate'
+    .pagination__container
+        p "page" {{ currentPage }} "of" {{ lastPage }}
+        p
+            button(@click="prev")="prev"
+            button(@click="next")="next"
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { usePagination } from 'vue-composable';
+import VueCompositionApi from '@vue/composition-api';
+
+Vue.use(VueCompositionApi);
+
+const {
+  currentPage, lastPage, next, prev, first, last,
+} = usePagination({
+  currentPage: 1,
+  pageSize: 10,
+  total: 50,
+});
 
 @Component({
 })
 
 export default class SingleMatch extends Vue {
+    next = next
+
+    prev = prev
+
+    first = first
+
+    last = last
+
+    currentPage = currentPage
+
+    lastPage = lastPage
 }
 </script>
 
