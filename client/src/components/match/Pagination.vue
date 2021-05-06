df99568856f3d7d5a22ed6532fe0772708a3286a
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-05-06 13:50:37 +0200

    automatic listing the pages works! - dirty

--- client/src/components/match/Pagination.vue
+++ client/src/components/match/Pagination.vue
@@ -9,28 +9,55 @@
 import {
   Component, Vue, Watch,
 } from 'vue-property-decorator';
+import axios from 'axios';
 import { usePagination } from 'vue-composable';
 import VueCompositionApi from '@vue/composition-api';
 
 Vue.use(VueCompositionApi);
 
-const {
-  currentPage, lastPage, next, prev,
-} = usePagination({
-  currentPage: 1,
-  pageSize: 10,
-  total: 50,
-});
-
 @Component
 export default class SingleMatch extends Vue {
-    nextPage = next
+    nextPage = '';
 
-    prevPage = prev
+    prevPage = '';
 
-    currentPage = currentPage
+    currentPage = '';
 
-    lastPage = lastPage
+    lastPage = '';
+
+    nicePage = '';
+
+    totalSize = '';
+
+    countMatches = async () => {
+      const results = await axios({
+        method: 'get',
+        url: 'http://localhost:3000/tournaments/countTournaments',
+      })
+        .then((data: any) => data.data);
+
+      return results;
+    }
+
+    async created() {
+      this.totalSize = await this.countMatches();
+
+      const {
+        currentPage, lastPage, next, prev,
+      } = usePagination({
+        currentPage: 1,
+        pageSize: 5,
+        total: this.totalSize,
+      });
+
+      this.nextPage = next;
+
+      this.prevPage = prev;
+
+      this.currentPage = currentPage;
+
+      this.lastPage = lastPage;
+    }
 
     @Watch('currentPage.value', { immediate: true, deep: true })
     loadTournaments = (newValue: string) => {
