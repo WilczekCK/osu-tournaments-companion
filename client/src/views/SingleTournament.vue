09879aa23e60ec21d62bd064656d320715c9f8ad
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-05-19 11:25:13 +0200

    added route + file for views

---
+++ client/src/views/SingleTournament.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  .content__container
+    .content__container--content
+      p {{$route.params.id}}
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue {
+}
+</script>
+
+<style lang="sass">
+.content__container
+  background: $bg-content
+  width: 1080px
+  min-height: 800px
+  display: flex
+  align-items: center
+  flex-direction: column
+  &--content
+    display: flex
+    flex-direction: column
+    width: 100%
+    align-items: center
+    position: relative
+    min-height: 900px
+    transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0)
+  &--header
+    padding: 25px 0px 50px
+@media (max-width: 1080px)
+  .content__container
+    width: 100%
+</style>
