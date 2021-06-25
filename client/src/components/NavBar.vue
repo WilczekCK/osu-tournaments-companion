c15ba4016c0350fa5745c9dd0b98424e1332fd9f
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-06-25 13:38:06 +0200

    other type of menu, no more dropdowns

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,27 +1,15 @@
 <template lang="pug">
-    .navigation__container(class="md-layout md-alignment-center-center")
-        .md-layout-item(class="md-size-10")
-        .navigation__container--wrapper(class="md-layout-item md-layout md-gutter")
-            router-link(to="/" class="md-layout-item md-size-77")
+    .navigation__container(class="md-layout md-gutter")
+        .navigation__container--wrapper(class="md-layout-item md-layout")
+            div(class="md-layout-item md-size-5")
+            router-link(to="/" class="md-layout-item md-size-80 logo")
                 ="osu!"
                 span="tc"
-            div(class="md-layout-item md-size-33 md-layout md-aligment-center-center")
-                 md-menu(md-size="medium" md-align-trigger @md-closed="isHamburgerActive = false" @md-opened="isHamburgerActive = true")
-                    md-button(md-menu-trigger)
-                        span(class="material-icons md-layout-item" v-if="!isHamburgerActive")
-                            ="menu"
-                        span(class="material-icons md-layout-item" v-else)
-                            ="menu_open"
-                    md-menu-content
-                        md-menu-item(name="sus" key="sus2")
-                            a(href="https://osupoli.pl")
-                                ="OsuPoli"
-                        md-menu-item(name="sus2" key="sus3")
-                            a(href="https://discord.gg/hErbYQ6Fs9")
-                                ="Discord"
-                        md-menu-item(name="sus3" key="sus4")
-                            a(href="https://github.com/wilczekck")
-                                ="Github"
+            div(class="md-layout-item md-size-10 menu")
+                a(href="#")
+                    img(:src="require(`@/assets/discord.svg`)")
+                a(href="#")
+                    img(:src="require(`@/assets/github.svg`)")
 </template>
 
 <script lang="ts">
@@ -44,6 +32,14 @@ export default class NavBar extends Vue {
         height: 100%
         display: flex
         align-content: center
+        .logo
+            margin-left: 10px
+        .menu
+            a
+                padding: 0px 5px
+                &:hover
+                    img
+                        filter: invert(60%) sepia(10%) saturate(7310%) hue-rotate(299deg) brightness(102%) contrast(101%)
         a
             color: $link-color !important
             font-size: 48px
@@ -52,19 +48,7 @@ export default class NavBar extends Vue {
                 color: $link-active
             &:hover
                 text-decoration: none
-        .md-menu
-            margin: 0 auto
-            &:hover
-                cursor: pointer
-            span
-                color: $link-color
-.md-list
-    .md-menu-item
-        padding: 5px 10px
-        a
-            color: $bg-content
-            font-weight: 600
-            &:hover
-                color: $link-active
-                text-decoration: none
+        img
+            width: 40px
+            height: 40px
 </style>
