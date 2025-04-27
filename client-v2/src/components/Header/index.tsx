6452ce192178550366c183852aa5fe8c22a7022c
Pawel <pawel@liveprice.pl>
2025-04-27 20:36:42 +0200

    config

---
+++ client-v2/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <div className='flex flex-row columns-2 w-full bg-container-main p-5 px-8 mt-4 rounded-xl'>
+            <h2 className='flex-grow-2'>osu!tc</h2>
+            <ul className='flex flex-row columns-4 gap-18'>
+                <li>Standard</li>
+                <li>Taiko</li>
+                <li>CTB</li>
+                <li>Mania</li>
+            </ul>
+        </div>
+    )
+}
--- client-v2/src/index.tsx
+++ client-v2/src/index.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import 'tailwindcss/tailwind.css'
 import Home from 'routes/Home'
 import Test from 'routes/Test'
 
@@ -8,10 +7,12 @@ const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
 root.render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="test" element={<Test />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="flex size-full">
+        <BrowserRouter>
+            <Routes>            
+                <Route path="/" element={<Home />} />
+                <Route path="test" element={<Test />} />
+            </Routes>
+        </BrowserRouter>
+    </div>
   );
