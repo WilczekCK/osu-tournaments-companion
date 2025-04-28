e1a44e2ff5eae7de5a76a0f9a88c8ecdf712d19e
Pawel <pawel@liveprice.pl>
2025-04-28 17:51:20 +0200

    little fixes

--- client-v2/src/components/Header/index.tsx
+++ client-v2/src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 export default function Header() {
-    return (
-        <div className='flex flex-row columns-2 w-full bg-container-main p-5 px-8 mt-4 rounded-xl'>
-            <h2 className='flex-grow-2'>osu!tc</h2>
-            <ul className='flex flex-row columns-4 gap-18'>
-                <li>Standard</li>
-                <li>Taiko</li>
-                <li>CTB</li>
-                <li>Mania</li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="flex flex-row columns-2 w-full bg-container-main p-5 px-8 mt-4 rounded-xl">
+      <h2 className="flex-grow-2">
+        <span className="text-white">osu!</span>
+        <span className="text-pink-500">tc</span>
+      </h2>
+      <ul className="flex flex-row columns-4 gap-18">
+        <li>Standard</li>
+        <li>Taiko</li>
+        <li>CTB</li>
+        <li>Mania</li>
+      </ul>
+    </div>
+  )
 }
--- client-v2/src/index.tsx
+++ client-v2/src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from 'routes/Home'
 import Test from 'routes/Test'
 
@@ -7,12 +7,12 @@ const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
 root.render(
-    <div className="flex size-full">
-        <BrowserRouter>
-            <Routes>            
-                <Route path="/" element={<Home />} />
-                <Route path="test" element={<Test />} />
-            </Routes>
-        </BrowserRouter>
-    </div>
-  );
+  <div className="flex size-full">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="test" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+)
