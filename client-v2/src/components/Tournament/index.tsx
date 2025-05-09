9f7b79e934d5f8ed81272fb9a3534505c8332329
Pawel <pawel@liveprice.pl>
2025-05-09 22:24:49 +0200

    padding fixes

--- client-v2/src/components/Tournament/index.tsx
+++ client-v2/src/components/Tournament/index.tsx
@@ -21,7 +21,7 @@ export default function Tournament({tournament}) {
   return (
     <>
       <div className="flex flex-col w-full px-2 rounded-xl items-center bg-container-tournament">
-        <div className="flex flex-row columns-2 w-full items-center gap-3 py-4 px-1">
+        <div className="flex flex-row columns-2 w-full items-center gap-3 py-4 px-1 pt-3">
           <Mode name={tournament.gameMode} displayText={false} />
           <div className="flex flex-col gap-0 items-start text-white text-sm grow">
             <div className="text-pink-900 font-semibold text-lg">{tournament.titleFlattened}</div>
@@ -71,7 +71,7 @@ export default function Tournament({tournament}) {
 
         <div
           className={`transition-[max-height] duration-300 ease-in-out overflow-hidden flex flex-row gap-2 w-full items-start text-white text-sm ${
-            isOpen ? 'max-h-[300px] py-2' : 'max-h-0 '
+            isOpen ? 'max-h-[300px] py-2 px-1' : 'max-h-0 '
           }`}
         >
           <TeamsList teams={teams}/>
