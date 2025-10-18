71a5840348194b9c7832a2320d7d363edffdea59
Pawel <pawel@liveprice.pl>
2025-10-18 14:37:50 +0200

    class -> className

--- client-v2/src/components/Tournament/Placeholder.tsx
+++ client-v2/src/components/Tournament/Placeholder.tsx
@@ -1,26 +1,26 @@
 export default function TournamentPlaceholder({itemNum, loadCount}) {
     return (
-        <div class="mx-auto w-full rounded-xl p-4 bg-container-tournament animate-fade">
+        <div className="mx-auto w-full rounded-xl p-4 bg-container-tournament animate-fade">
             {(loadCount === 1 && itemNum <= 1) && (
-                <div class="flex animate-pulse items-center h-[96px] w-[105.5%] mt-[-16px] ml-[-16px] pb-5">
-                    <div class="flex-1">
-                        <div class="col-span-2 h-20 bg-gray-300 rounded-xl rounded-b-none"></div>     
+                <div className="flex animate-pulse items-center h-[96px] w-[105.5%] mt-[-16px] ml-[-16px] pb-5">
+                    <div className="flex-1">
+                        <div className="col-span-2 h-20 bg-gray-300 rounded-xl rounded-b-none"></div>     
                     </div>
                 </div>
             )}
             
-            <div class="flex animate-pulse space-x-4 items-center">
-                <div class="size-10 rounded-full bg-gray-300"></div>    
-                <div class="flex-1 space-y-6 py-1">
-                        <div class="space-y-3">
-                            <div class="grid grid-cols-4 gap-4">
-                                <div class="col-span-1 h-2 rounded bg-pink-900"></div>
+            <div className="flex animate-pulse space-x-4 items-center">
+                <div className="size-10 rounded-full bg-gray-300"></div>    
+                <div className="flex-1 space-y-6 py-1">
+                        <div className="space-y-3">
+                            <div className="grid grid-cols-4 gap-4">
+                                <div className="col-span-1 h-2 rounded bg-pink-900"></div>
                             </div>
-                            <div class="grid grid-cols-4 gap-4">
-                                <div class="col-span-2 h-2 rounded bg-gray-300"></div>     
+                            <div className="grid grid-cols-4 gap-4">
+                                <div className="col-span-2 h-2 rounded bg-gray-300"></div>     
                             </div>
-                            <div class="grid grid-cols-6 gap-4">
-                                <div class="col-span-2 h-2 rounded bg-gray-300"></div>     
+                            <div className="grid grid-cols-6 gap-4">
+                                <div className="col-span-2 h-2 rounded bg-gray-300"></div>     
                             </div>
                         </div>
                 </div>
